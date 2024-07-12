a6a61208054d3a3b2b5599f640117f2223088290
karl-jerico <jerico.jeks@gmail.com>
2024-07-12 10:06:15 +0800

    feat/HHA-38: Add table for address

---
+++ database/migrations/2024_07_10_130521_create_user_address_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->string('province');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('barangay');
+            $table->text('line')->nullable();
+            $table->string('landline')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+}
